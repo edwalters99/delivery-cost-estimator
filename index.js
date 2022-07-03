1588ebd335aa7111988094543c8a1b4dd24e468a
edwalters99 <69717169+edwalters99@users.noreply.github.com>
2022-07-03 16:35:08 +1000

    Private class fields implemented

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const processInput = require("./processInput.js");
 const offersData = require("./offers.json");
 
-// Make sure we got a filename on the command line.
+// Ensure filename is supplied from the command line
 if (process.argv.length < 3) {
   console.log("Usage: node " + process.argv[1] + " FILENAME");
   process.exit(1);
 }
-// Read the file and print its contents.
+
 const fs = require("fs"),
   filename = process.argv[2];
 fs.readFile(filename, "utf8", function (err, data) {
--- package.js
+++ package.js
@@ -1,33 +1,50 @@
 class Package {
-  constructor(pkgID, pkgWeightInKg, pkgDistanceInKm, baseDeliveryCost, offerCode) {
-    this.pkgID = pkgID;
-    this.pkgWeightInKg = pkgWeightInKg;
-    this.pkgDistanceInKm = pkgDistanceInKm;
-    this.baseDeliveryCost = baseDeliveryCost;
-    this.offerCode = offerCode;
-    this.discount = 0;
-    this.totalExDiscount =
+  #pkgID;
+  #pkgWeightInKg;
+  #pkgDistanceInKm;
+  #offerCode;
+  #discount;
+  #totalExDiscount;
+  #total;
+
+  constructor(
+    pkgID,
+    pkgWeightInKg,
+    pkgDistanceInKm,
+    baseDeliveryCost,
+    offerCode
+  ) {
+    this.#pkgID = pkgID;
+    this.#pkgWeightInKg = pkgWeightInKg;
+    this.#pkgDistanceInKm = pkgDistanceInKm;
+    this.#offerCode = offerCode;
+    this.#discount = 0;
+    this.#totalExDiscount =
       baseDeliveryCost + pkgWeightInKg * 10 + pkgDistanceInKm * 5;
-    this.total = this.totalExDiscount - this.discount;
+    this.#total = this.#totalExDiscount - this.#discount;
   }
 
   getSummary() {
-    return `${this.pkgID} ${this.discount} ${this.total}`;
+    return `${this.#pkgID} ${this.#discount} ${this.#total}`;
   }
 
   checkDiscountValid(offersData) {
-    if (!this.offerCode || !offersData[this.offerCode] || this.discount !== 0) {
-      return false;
-    }
     if (
-      offersData[this.offerCode]["distanceMin"] > this.pkgDistanceInKm ||
-      offersData[this.offerCode]["distanceMax"] < this.pkgDistanceInKm
+      !this.#offerCode ||
+      !offersData[this.#offerCode] ||
+      this.#discount !== 0
     ) {
       return false;
     }
     if (
-      offersData[this.offerCode]["weightMin"] > this.pkgWeightInKg ||
-      offersData[this.offerCode]["weightMax"] < this.pkgWeightInKg
+      offersData[this.#offerCode]["distanceMin"] > this.#pkgDistanceInKm ||
+      offersData[this.#offerCode]["distanceMax"] < this.#pkgDistanceInKm
+    ) {
+      return false;
+    }
+    if (
+      offersData[this.#offerCode]["weightMin"] > this.#pkgWeightInKg ||
+      offersData[this.#offerCode]["weightMax"] < this.#pkgWeightInKg
     ) {
       return false;
     }
@@ -36,11 +53,10 @@ class Package {
   }
 
   applyDiscount(offersData) {
-    this.discount =
-      (this.totalExDiscount * offersData[this.offerCode]["discountPercent"]) / 100;
-    this.total = this.totalExDiscount - this.discount;
-    // console.log(this.discount)
-    // console.log(this.total)
+    this.#discount =
+      (this.#totalExDiscount * offersData[this.#offerCode]["discountPercent"]) /
+      100;
+    this.#total = this.#totalExDiscount - this.#discount;
   }
 }
 
